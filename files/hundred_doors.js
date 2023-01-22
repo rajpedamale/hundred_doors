b9ec87be49b6504bbd618a0841c1a0d5c0c34d26
la6YtE <la6YtE@cyber-dojo.org>
2023-01-22 12:01:47 +0000

    26 ran tests, predicted red, got amber

--- files/hundred_doors.js
+++ files/hundred_doors.js
@@ -3,13 +3,13 @@
 const open = '@';
 const closed = '#';
 
-function visitDoors(visitCount){
-  return doors.map((door, doorNumber) => door===closed?open:closed);
-}
 function hundredDoors(doors, visitCount=0) {
   let updatedDoors = [...doors];
   for(let i=1; i<=visitCount; i++){
-    updatedDoors = visitDoors(i);      
+    updatedDoors = 
+      updatedDoors.map(
+        (door, doorNumber) => door===closed?open:closed);
+      )
   }
   return updatedDoors.join('');
 }
